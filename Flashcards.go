76b228aee1b501f290b35ff6aa637c428440f20d
AleLoiac <alessandroloiac@gmail.com>
2023-02-05 23:47:54 +0100

    05/02

--- Flashcards.go
+++ Flashcards.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+var card string
+var definition string
+
 func main() {
+
+	fmt.Println("Card:")
+
+	fmt.Scan(&card)
+
+	fmt.Println("Definition:")
+
+	fmt.Scan(&definition)
 
 }
